5fe5bbcfa7dd9deed78b2f09e63c39f7180fee42
milunusu <milanesaruggiero@gmail.com>
2024-03-15 19:26:13 -0400

    button1

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.get('/', (req, res) => {
             <meta property="fc:frame:image" content="https://maroon-able-dolphin-503.mypinata.cloud/ipfs/QmW9hoQXMaqs9U29dWiZWujPmq1NkPkinqr6xruTjg8tMw">
             <meta property="fc:frame:image:aspect_ratio" content="1.91:1">
 
-            <meta property="fc:frame" content="vNext">
-            <meta property="fc:frame:button1" content="ok alright">
+            
+            <meta property="fc:frame:button1" content="link">
             <meta property="fc:frame:button1:action" content="link">
-            <meta property="fc:frame:button1:target" content="https://maroon-able-dolphin-503.mypinata.cloud/ipfs/QmPRmhLgJUETG2SDDrNDSiaiztDPF2EWYsbkGFmEzytxq7">
+            <meta property="fc:frame:button1:target" content="https://maroon-able-dolphin-503.mypinata.cloud/ipfs/QmPRmhLgJUETG2SDDrNDSiaiztDPF2EWYsbkGFmEzytxq7" />
             
             
            
